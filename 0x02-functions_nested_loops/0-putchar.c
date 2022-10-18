2ae14370393ffa9be8c26d2c906bfd6ba8acd462
Abdurrahman-X <abolajiabdurrahman@gmail.com>
2022-10-18 06:08:26 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/** main - print _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *c = "_putchar";
+
+	while (*c)
+	{
+		_putchar(*c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
