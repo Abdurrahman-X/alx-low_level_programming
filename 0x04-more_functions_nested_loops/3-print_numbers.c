1089ee5c914bdc2307906bd18d2f3f67229c1a2a
Abdurrahman-X <abolajiabdurrahman@gmail.com>
2022-10-20 09:39:59 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,18 +2,15 @@
 
 /**
  * print_numbers - print numbers 0 - 9
- * Return: 0;
  */
 
 void print_numbers(void)
 {
 	int n;
 
-	for ( n = 0; n < 10; n++)
+	for (n = 0; n < 10; n++)
 	{
 		_putchar(n);
 	}
 	_putchar('\n');
-
-	return (0);
 }
