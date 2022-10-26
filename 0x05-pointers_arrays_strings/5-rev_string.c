23ab0e048b2bdb2f0998b786da15af61ab03e5f3
Abdurrahman-X <abolajiabdurrahman@gmail.com>
2022-10-26 07:41:35 -0700

    reverse_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,10 +11,10 @@ void rev_string(char *s)
 	int i;
 	int count = 0;
 
-	while (s[fcounter] != '\0')
+	while (s[count] != '\0')
 		count++;
 
-	for (i = 0; i < fcounter; i++)
+	for (i = 0; i < count; i++)
 	{
 		count--;
 		tmp = s[i];
