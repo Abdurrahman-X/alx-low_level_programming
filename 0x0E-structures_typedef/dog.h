b0b99787fc588c3dbdb70326bb3e128a86617a9e
Abdurrahman-X <abolajiabdurrahman@gmail.com>
2022-11-15 12:44:36 -0800

    fixed dog.h error

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,26 +2,20 @@
 #define DOG_H
 
 /**
- * dog_t - Typedef for struct dog
- */
-
-typedef struct dog dog_t;
-
-/**
  * struct dog - structure for dogs
  * @name: first member
  * @age: second member
  * @owner: third member
  */
 
-struct dog
+typedef struct dog
 {
 
 	char *name;
 	float age;
 	char *owner;
 
-};
+} dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
