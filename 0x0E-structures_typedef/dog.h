c7d905e249f8c1d2907ed0612a294b8869f7355a
Abdurrahman-X <abolajiabdurrahman@gmail.com>
2022-11-15 12:36:35 -0800

    fixed dog.h error

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,14 +16,18 @@ typedef struct dog dog_t;
 
 struct dog
 {
+
 	char *name;
 	float age;
 	char *owner;
+
 };
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-ivoid free_dog(dog_t *d);
+void free_dog(dog_t *d);
+
 
 #endif
